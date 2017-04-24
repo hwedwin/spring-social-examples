82c4d431d92237dbdbc0808c2cae340df1802026
tanghuan <tanghuanyou@163.com>
2017-04-24 18:10:29 +0800

    和Spring Data JPA集成

--- src/main/java/com/tanghuan/dev/oauth/repository/UserConnectionRepository.java
+++ src/main/java/com/tanghuan/dev/oauth/repository/UserConnectionRepository.java
@@ -3,6 +3,7 @@ package com.tanghuan.dev.oauth.repository;
 import com.tanghuan.dev.oauth.entity.domain.UserConnection;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by Arthur on 2017/4/24.
@@ -11,12 +12,15 @@ public interface UserConnectionRepository extends BaseRepository<UserConnection>
 
     List<UserConnection> findByProviderId(String providerId);
 
-    List<UserConnection> findByUserIdAndProviderIdOrderByRankAsc(String userId, String providerId);
+    List<UserConnection> findByProviderIdAndProviderUserId(String providerId, String providerUserId);
+
+    List<UserConnection> findByProviderIdAndProviderUserIdIn(String providerId, Set<String> providerUserIds);
 
     List<UserConnection> findByUserIdOrderByProviderIdAscRankAsc(String userId);
+
+    List<UserConnection> findByUserIdAndProviderIdOrderByRankAsc(String userId, String providerId);
 
     UserConnection findByUserIdAndProviderIdAndProviderUserId(String userId, String providerId, String providerUserId);
 
     UserConnection findFirstByUserIdAndProviderIdOrderByRankDesc(String userId, String providerId);
-
 }
--- src/main/java/com/tanghuan/dev/oauth/security/repo/UsersConnectionRepositoryImpl.java
+++ src/main/java/com/tanghuan/dev/oauth/security/repo/UsersConnectionRepositoryImpl.java
@@ -1,13 +1,15 @@
 package com.tanghuan.dev.oauth.security.repo;
 
+import com.tanghuan.dev.oauth.entity.domain.UserConnection;
 import com.tanghuan.dev.oauth.repository.UserConnectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.connect.Connection;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.social.connect.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Arthur on 2017/4/24.
@@ -17,18 +19,58 @@ public class UsersConnectionRepositoryImpl implements UsersConnectionRepository 
     @Autowired
     private UserConnectionRepository userConnectionRepository;
 
+    private final ConnectionFactoryLocator connectionFactoryLocator;
+
+    private final TextEncryptor textEncryptor;
+
+    private ConnectionSignUp connectionSignUp;
+
+    public UsersConnectionRepositoryImpl(ConnectionFactoryLocator connectionFactoryLocator, TextEncryptor textEncryptor) {
+        this.connectionFactoryLocator = connectionFactoryLocator;
+        this.textEncryptor = textEncryptor;
+    }
+
     @Override
     public List<String> findUserIdsWithConnection(Connection<?> connection) {
-        return null;
+        ConnectionKey connectionKey = connection.getKey();
+
+        List<UserConnection> userConnections = userConnectionRepository.findByProviderIdAndProviderUserId(connectionKey.getProviderId(), connectionKey.getProviderUserId());
+
+        if (userConnections == null || userConnections.size() == 0) return null;
+
+        List<String> localUserIds = userConnections.stream().map(UserConnection::getUserId).distinct().collect(Collectors.toList());
+
+        if (localUserIds.size() == 0 && connectionSignUp != null) {
+            String newUserId = connectionSignUp.execute(connection);
+            if (newUserId != null)
+            {
+                createConnectionRepository(newUserId).addConnection(connection);
+                return Arrays.asList(newUserId);
+            }
+        }
+
+        return localUserIds;
     }
 
     @Override
     public Set<String> findUserIdsConnectedTo(String providerId, Set<String> providerUserIds) {
-        return null;
+
+        List<UserConnection> userConnections = userConnectionRepository.findByProviderIdAndProviderUserIdIn(providerId, providerUserIds);
+
+        if (userConnections == null || userConnections.size() == 0) return null;
+
+        Set<String> localUserIds = userConnections.stream().map(UserConnection::getUserId).distinct().collect(Collectors.toSet());
+
+        return localUserIds;
     }
 
     @Override
     public ConnectionRepository createConnectionRepository(String userId) {
-        return null;
+        return new ConnectionRepositoryImpl(userId, connectionFactoryLocator, textEncryptor);
     }
+
+    public void setConnectionSignUp(ConnectionSignUp connectionSignUp) {
+        this.connectionSignUp = connectionSignUp;
+    }
+
 }
