15770136bda586eeed9e89031fff8aebab360d53
tanghuan <tanghuanyou@163.com>
2017-04-25 22:01:25 +0800

    去除了表单登录和basic登录

--- src/main/java/com/tanghuan/dev/oauth/config/WebSecurityConfig.java
+++ src/main/java/com/tanghuan/dev/oauth/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import com.tanghuan.dev.oauth.security.uds.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
